c62176787a9ae4e287c4ef85e28f96abdd97a770
kondziku6 <kkaczorowski06@gmail.com>
2026-05-18 21:43:46 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,144 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+const nums = document.querySelectorAll('.num');
+const backspace = document.querySelector('#backspace');
+const clearCurrentNum = document.querySelector('#clearCurrentNum');
+const clearAll = document.querySelector('#clearAll');
+const operations = document.querySelectorAll('.operation');
+let currentNum = parseFloat(display.textContent);
+let storedNum = undefined;
+let toDoOperation = '';
+let isEqualActive = false;
+let isCurrentNumWrote = false;
+
+nums.forEach(num => {
+    num.addEventListener('click', () => {
+        if(isCurrentNumWrote === false || isEqualActive){
+            currentNum = 0;
+        }
+        if(isEqualActive){
+            toDoOperation = '';
+            storedNum = undefined;
+        }
+        if(display.textContent === 'Do not divide by zero'){
+            buttons.forEach(button => {
+                button.disabled = false;
+            });
+        }
+        if(currentNum < 100000000000000){
+            currentNum = parseFloat(currentNum) * 10 + parseFloat(num.textContent);
+            display.textContent = currentNum;
+        }
+        isEqualActive = false;
+        isCurrentNumWrote = true;
+    });
+}); // Writing current number on display
+backspace.addEventListener('click', () => {
+    if(isCurrentNumWrote || isEqualActive){
+        if(display.textContent === 'Do not divide by zero'){
+            buttons.forEach(button => {
+                button.disabled = false;
+            });
+        }
+        currentNum = String(currentNum).substring(0, String(currentNum).length - 1);
+        if(isEqualActive){
+            toDoOperation = '';
+            storedNum = undefined;
+            currentNum = 0;
+        }
+        if(currentNum == ''){
+            currentNum = 0;
+        } // If current number is empty show on display 0
+        display.textContent = currentNum;
+    }
+}); // Deleting last writed number
+clearCurrentNum.addEventListener('click', () => {
+    if(display.textContent === 'Do not divide by zero'){
+        buttons.forEach(button => {
+            button.disabled = false;
+        });
+    }
+    if(isEqualActive){
+        toDoOperation = '';
+        storedNum = undefined;
+    }
+    currentNum = 0;
+    display.textContent = currentNum;
+}); // Reseting current number
+clearAll.addEventListener('click', () => {
+    if(display.textContent === 'Do not divide by zero'){
+        buttons.forEach(button => {
+            button.disabled = false;
+        });
+    }
+    toDoOperation = '';
+    storedNum = undefined;
+    currentNum = 0;
+    display.textContent = currentNum;
+}); // Reseting whole calculator
+operations.forEach(operation => {
+    operation.addEventListener('click', () => {
+        if(toDoOperation === '/' && currentNum === 0){
+            toDoOperation = '';
+            isEqualActive = true;
+            buttons.forEach(button => {
+                button.disabled = true;
+            });
+            nums.forEach(num => {
+                num.disabled = false;
+            });
+            clearCurrentNum.disabled = false;
+            clearAll.disabled = false;
+            backspace.disabled = false;
+            for(op of operations){
+                if(op.textContent === '='){
+                    op.disabled = false;
+                }
+            }
+            display.textContent = 'Do not divide by zero';
+        }
+        else{
+            isEqualActive = false;
+            if(toDoOperation !== '' && storedNum !== undefined && (isCurrentNumWrote || operation.textContent === '=')){
+                switch(toDoOperation){
+                    case '/': storedNum /= currentNum;
+                    break;
+                    case 'X': storedNum *= currentNum;
+                    break;
+                    case '-': storedNum -= currentNum;
+                    break;
+                    case '+': storedNum += currentNum;
+                }
+            }
+            else if(toDoOperation === '' && storedNum === undefined){
+                storedNum = currentNum;
+            }
+            switch(operation.textContent){
+                case '/': toDoOperation = '/';
+                break;
+                case 'X': toDoOperation = 'X';
+                break;
+                case '-': toDoOperation = '-';
+                break;
+                case '+': toDoOperation = '+';
+                break;
+                case '=': {
+                    isEqualActive = true;
+                    if(display.textContent === 'Do not divide by zero'){
+                        toDoOperation = '';
+                        storedNum = 0;
+                        currentNum = 0;
+                        buttons.forEach(button => {
+                            button.disabled = false;
+                        });
+                    }
+                }
+            }
+            if(operation.textContent !== '='){
+                currentNum = storedNum;
+            }
+            display.textContent = storedNum; 
+            isCurrentNumWrote = false;
+        }
+    });
+}); // Operations for addition, subtraction, multiplication, division
